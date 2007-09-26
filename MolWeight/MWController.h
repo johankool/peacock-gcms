aab57ddc8ce7625934e803ad814c005c91b6c215
jkool <jkool@d5e6421c-dc4f-0410-8a52-2d0b16c40e6d>
2007-09-26 22:31:35 +0000

    move to trunk

---
+++ MolWeight/MWController.h
@@ -0,0 +1,30 @@
+//
+//  This file is part of the application with the working name:
+//  Peacock
+//
+//  Created by Johan Kool.
+//  Copyright (c) 2003-2004 Johan Kool. All rights reserved.
+//
+
+@interface MWController : NSWindowController {
+    IBOutlet id contents;
+    IBOutlet id formula;
+    IBOutlet id lowerCase;
+    IBOutlet id status;
+    IBOutlet id statusIcon;
+    IBOutlet id weight;
+    IBOutlet id panelWindow;
+}
+- (IBAction)calculate:(id)sender;
+- (IBAction)clear:(id)sender;
+- (void)showError:(BOOL)input;
+- (IBAction)openPanel:(id)sender;
+
+@property (retain) id panelWindow;
+@property (retain) id lowerCase;
+@property (retain) id formula;
+@property (retain) id weight;
+@property (retain) id statusIcon;
+@property (retain) id contents;
+@property (retain) id status;
+@end
---
+++ jk_statistics.c
@@ -0,0 +1,51 @@
+/*
+ *  jk_statistics.c
+ *  Peacock
+ *
+ *  Created by Johan Kool on 10-4-06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "jk_statistics.h"
+#include "math.h"
+
+#pragma mark FUNCTIONS
+
+void normalize(float *input, int count) {
+    int i;
+	float maximum;
+	
+	maximum = fabsf(input[0]);
+	for (i = 1; i < count; i++) {
+		if (fabsf(input[i]) > maximum) maximum = fabsf(input[i]);
+	}
+	
+	for (i = 0; i < count; i++) {
+		input[i] = input[i]/maximum;
+	}
+}
+
+float jk_stats_float_min(float *data, int count) {
+	int i;
+	float min = data[0];
+	
+	for (i=0; i < count; i++) {
+		if (data[i] < min) {
+			min = data[i];
+		}
+	}
+	return min;
+}
+
+float jk_stats_float_max(float *data, int count) {
+	int i;
+	float max = data[0];
+	
+	for (i=0; i < count; i++) {
+		if (data[i] > max) {
+			max = data[i];
+		}
+	}
+	return max;
+}
