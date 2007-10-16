edb8d70dcc671401e460b63808b2b3ad4d66f266
jkool <jkool@d5e6421c-dc4f-0410-8a52-2d0b16c40e6d>
2007-10-16 13:40:59 +0000

    toolbar

--- PKWindowController.m
+++ PKWindowController.m
@@ -19,26 +19,178 @@
     [[(JKAppDelegate *)[NSApp delegate] summaryController] setWindow:[self window]];
     [[documentTabView tabViewItemAtIndex:1] setView:[[[(JKAppDelegate *)[NSApp delegate] ratiosController] window] contentView]];
     [[(JKAppDelegate *)[NSApp delegate] ratiosController] setWindow:[self window]];
+    [self setupToolbar];
 }
 
 - (void)tabView:(NSTabView *)tabView didSelectTabViewItem:(NSTabViewItem *)tabViewItem
 {
     if ([[tabViewItem identifier] isKindOfClass:[JKGCMSDocument class]]) {
+        [[tabViewItem identifier] addWindowController:self];
         [[self window] setTitleWithRepresentedFilename:[[tabViewItem identifier] fileName]];
         [[self window] setNextResponder:[[tabViewItem identifier] mainWindowController]];
         [[[tabViewItem identifier] mainWindowController] setWindow:[self window]];
-        [self setDocument:[tabViewItem identifier]];
-    } else if ([[tabViewItem identifier] isEqualToString:@"summary"]) {
-        [[self window] setTitleWithRepresentedFilename:@""];
+     } else if ([[tabViewItem identifier] isEqualToString:@"summary"]) {
+        [[self document] removeWindowController:self];
         [[self window] setTitle:@"Summary"];
         [[self window] setNextResponder:(NSResponder *)[(JKAppDelegate *)[NSApp delegate] summaryController]];
-        [self setDocument:nil];
     } else if ([[tabViewItem identifier] isEqualToString:@"ratios"]) {
-        [[self window] setTitleWithRepresentedFilename:@""];
+        [[self document] removeWindowController:self];
         [[self window] setTitle:@"Ratios"];
         [[self window] setNextResponder:(NSResponder *)[(JKAppDelegate *)[NSApp delegate] ratiosController]];
-        [self setDocument:nil];
 	}
 }
+
+#pragma mark NSToolbar Management
+
+- (void)setupToolbar {
+    // Create a new toolbar instance, and attach it to our document window 
+    NSToolbar *toolbar = [[[NSToolbar alloc] initWithIdentifier: @"PeacockAllInOneWindowToolbarIdentifier"] autorelease];
+    
+    // Set up toolbar properties: Allow customization, give a default display mode, and remember state in user defaults 
+    [toolbar setVisible:YES];
+    [toolbar setAllowsUserCustomization: YES];
+    [toolbar setAutosavesConfiguration: YES];
+	
+    // We are the delegate
+    [toolbar setDelegate: self];
+    
+    // Attach the toolbar to the document window 
+    [[self window] setToolbar:toolbar];
+}
+
+- (NSToolbarItem *) toolbar: (NSToolbar *)toolbar itemForItemIdentifier: (NSString *) itemIdent willBeInsertedIntoToolbar:(BOOL) willBeInserted {
+    // Required delegate method:  Given an item identifier, this method returns an item 
+    // The toolbar will use this method to obtain toolbar items that can be displayed in the customization sheet, or in the toolbar itself 
+    NSToolbarItem *toolbarItem = [[[NSToolbarItem alloc] initWithItemIdentifier: itemIdent] autorelease];
+    
+    if ([itemIdent isEqual: @"Save Document Item Identifier"]) {
+		// Set the text label to be displayed in the toolbar and customization palette 
+		[toolbarItem setLabel:NSLocalizedString(@"Save",@"")];
+		[toolbarItem setPaletteLabel:NSLocalizedString(@"Save",@"")];
+		
+		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+		[toolbarItem setToolTip:NSLocalizedString(@"Save Your Document",@"")];
+		[toolbarItem setImage: [NSImage imageNamed:@"save_document"]];
+		
+		// Tell the item what message to send when it is clicked 
+		[toolbarItem setTarget: [self document]];
+		[toolbarItem setAction: @selector(saveDocument:)];
+    }  else if ([itemIdent isEqual: @"Identify Baseline Item Identifier"]) {
+		// Set the text label to be displayed in the toolbar and customization palette 
+		[toolbarItem setLabel:NSLocalizedString(@"Identify Baseline",@"")];
+		[toolbarItem setPaletteLabel:NSLocalizedString(@"Identify Baseline",@"")];
+		
+		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+		[toolbarItem setToolTip:NSLocalizedString(@"Identify the baseline in your chromatogram",@"")];
+		[toolbarItem setImage: [NSImage imageNamed: @"Identify Baseline"]];
+		
+		// Tell the item what message to send when it is clicked 
+//		[toolbarItem setTarget: self];
+		[toolbarItem setAction: @selector(obtainBaseline:)];
+    }   else if ([itemIdent isEqual: @"Identify Peaks Item Identifier"]) {
+		// Set the text label to be displayed in the toolbar and customization palette 
+		[toolbarItem setLabel:NSLocalizedString(@"Identify Peaks",@"")];
+		[toolbarItem setPaletteLabel:NSLocalizedString(@"Identify Peaks",@"")];
+		
+		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+		[toolbarItem setToolTip:NSLocalizedString(@"Identify the peaks in your chromatogram",@"")];
+		[toolbarItem setImage: [NSImage imageNamed: @"Identify Peaks"]];
+		
+		// Tell the item what message to send when it is clicked 
+//		[toolbarItem setTarget: self];
+		[toolbarItem setAction: @selector(identifyPeaks:)];
+    } else if ([itemIdent isEqual: @"Identify Compounds Item Identifier"]) {
+		// Set the text label to be displayed in the toolbar and customization palette 
+		[toolbarItem setLabel:NSLocalizedString(@"Identify Compounds",@"")];
+		[toolbarItem setPaletteLabel:NSLocalizedString(@"Identify Compounds",@"")];
+		
+		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+		[toolbarItem setToolTip:NSLocalizedString(@"Identify the compounds associated with the peaks in your chromatogram",@"")];
+		[toolbarItem setImage: [NSImage imageNamed: @"Identify Compounds"]];
+		
+		// Tell the item what message to send when it is clicked 
+//		[toolbarItem setTarget: self];
+		[toolbarItem setAction: @selector(identifyCompounds:)];
+    } else if ([itemIdent isEqual: @"Inspector"]) {
+		// Set the text label to be displayed in the toolbar and customization palette 
+		[toolbarItem setLabel:NSLocalizedString(@"Inspector",@"")];
+		[toolbarItem setPaletteLabel:NSLocalizedString(@"Inspector",@"")];
+		
+		// Set up a reasonable tooltip, and image   Note, these aren't localized, but you will likely want to localize many of the item's properties 
+		[toolbarItem setToolTip:NSLocalizedString(@"Show an inspector to change attributs of the selected object",@"")];
+		[toolbarItem setImage: [NSImage imageNamed: @"info"]];
+		
+		// Tell the item what message to send when it is clicked 
+		//	[toolbarItem setTarget: self];
+		[toolbarItem setAction: @selector(showInspector:)];
+    }   else {
+		// itemIdent refered to a toolbar item that is not provide or supported by us or cocoa 
+		// Returning nil will inform the toolbar this kind of item is not supported 
+		toolbarItem = nil;
+    }
+    return toolbarItem;
+}
+
+- (NSArray *) toolbarDefaultItemIdentifiers: (NSToolbar *) toolbar {
+    // Required delegate method:  Returns the ordered list of items to be shown in the toolbar by default    
+    // If during the toolbar's initialization, no overriding values are found in the user defaults, or if the
+    // user chooses to revert to the default items this set will be used 
+    return [NSArray arrayWithObjects:	@"Save Document Item Identifier", NSToolbarPrintItemIdentifier, NSToolbarFlexibleSpaceItemIdentifier, @"Identify Baseline Item Identifier", @"Identify Peaks Item Identifier", @"Identify Compounds Item Identifier", NSToolbarFlexibleSpaceItemIdentifier, 
+        NSToolbarShowColorsItemIdentifier, NSToolbarShowFontsItemIdentifier, NSToolbarSeparatorItemIdentifier, @"Inspector", nil];
+}
+
+- (NSArray *) toolbarAllowedItemIdentifiers: (NSToolbar *) toolbar {
+    // Required delegate method:  Returns the list of all allowed items by identifier.  By default, the toolbar 
+    // does not assume any items are allowed, even the separator.  So, every allowed item must be explicitly listed   
+    // The set of allowed items is used to construct the customization palette 
+    return [NSArray arrayWithObjects:  @"Identify Baseline Item Identifier", @"Identify Peaks Item Identifier", @"Identify Compounds Item Identifier",  @"Save Document Item Identifier", NSToolbarPrintItemIdentifier, @"Inspector",
+        NSToolbarShowColorsItemIdentifier, NSToolbarShowFontsItemIdentifier, NSToolbarCustomizeToolbarItemIdentifier,
+        NSToolbarFlexibleSpaceItemIdentifier, NSToolbarSpaceItemIdentifier, NSToolbarSeparatorItemIdentifier, nil];
+}
+
+- (void) toolbarWillAddItem: (NSNotification *) notif {
+    // Optional delegate method:  Before an new item is added to the toolbar, this notification is posted.
+    // This is the best place to notice a new item is going into the toolbar.  For instance, if you need to 
+    // cache a reference to the toolbar item or need to set up some initial state, this is the best place 
+    // to do it.  The notification object is the toolbar to which the item is being added.  The item being 
+    // added is found by referencing the @"item" key in the userInfo 
+    NSToolbarItem *addedItem = [[notif userInfo] objectForKey: @"item"];
+    if ([[addedItem itemIdentifier] isEqual: NSToolbarPrintItemIdentifier]) {
+		[addedItem setToolTip:NSLocalizedString(@"Print Your Document",@"")];
+		//	[addedItem setTarget: self];
+    }
+}  
+
+- (void) toolbarDidRemoveItem: (NSNotification *) notif {
+    // Optional delegate method:  After an item is removed from a toolbar, this notification is sent.   This allows 
+    // the chance to tear down information related to the item that may have been cached.   The notification object
+    // is the toolbar from which the item is being removed.  The item being added is found by referencing the @"item"
+    // key in the userInfo 
+	//    NSToolbarItem *removedItem = [[notif userInfo] objectForKey: @"item"];
+	
+}
+
+- (BOOL) validateToolbarItem: (NSToolbarItem *) toolbarItem {
+    // Optional method:  This message is sent to us since we are the target of some toolbar item actions 
+    // (for example:  of the save items action) 
+    BOOL enable = NO;
+    if ([[toolbarItem itemIdentifier] isEqual: @"Save Document Item Identifier"]) {
+		// We will return YES (ie  the button is enabled) only when the document is dirty and needs saving 
+		enable = [[self document] isDocumentEdited];
+		enable = YES;
+    } else if ([[toolbarItem itemIdentifier] isEqual: NSToolbarPrintItemIdentifier]) {
+		enable = YES;
+    }else if ([[toolbarItem itemIdentifier] isEqual: @"Identify Baseline Item Identifier"]) {
+		enable = YES;
+    }else if ([[toolbarItem itemIdentifier] isEqual: @"Identify Peaks Item Identifier"]) {
+		enable = YES;
+    }else if ([[toolbarItem itemIdentifier] isEqual: @"Identify Compounds Item Identifier"]) {
+		enable = YES;
+    }else if ([[toolbarItem itemIdentifier] isEqual: @"Inspector"]) {
+		enable = YES;
+    }
+    return enable;
+}
+#pragma mark -
 
 @end
